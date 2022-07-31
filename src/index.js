949e266822ef3e723c1a99a1cb3c4421d76ce6f0
dbataeva <b.daffa.v@yandex.ru>
2022-07-31 13:37:32 +0300

    feat: decode works

--- src/index.js
+++ src/index.js
@@ -38,7 +38,30 @@ const MORSE_TABLE = {
 };
 
 function decode(expr) {
-    // write your solution here
+    const letters = expr.split('*').filter(word => word ? word : undefined).map(word => {
+        let oneWordLetters = [];
+
+        for (let i = 0; i < word.length; i += 10) {
+            oneWordLetters.push(word.slice(i, i + 10));
+        }
+
+        const onlyChars = oneWordLetters.map(letter => letter.slice(letter.indexOf('1')));
+        let dotAndDashWord = [];
+
+        onlyChars.forEach(letter => {
+            let oneChar = [];
+
+            for (let i = 0; i < letter.length; i += 2) {
+                oneChar.push(letter.slice(i, i + 2));
+            }
+
+            dotAndDashWord.push(oneChar.map(code => code === '10' ? '.' : '-').join(''));
+        });
+
+        return dotAndDashWord.map(letter => MORSE_TABLE[letter]).join('');
+    });
+
+    return letters.join(' ');
 }
 
 module.exports = {
